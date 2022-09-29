c9650885e2b458571ebe9eb7ee91ff20c8a8f66c
apple <apple@xuhongbiaodeMacBook-Air.local>
2022-09-29 16:44:42 +0800

    DBNet文字分割算法

---
+++ DBNet.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+import torch
+
+class DBNet: # 文字分割类
+    def __init__(self,args):
+        pass
+    def load_image(self,image_path):
+        pass
+    def resize_image(self,image_path):
+        pass
+def segmentation(): # 文字分割函数
+    model = DBNet(args)
+if __name__ == '__main__':
+    segmentation()
